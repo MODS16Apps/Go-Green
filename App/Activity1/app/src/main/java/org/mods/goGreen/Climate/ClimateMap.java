09c81383e01e13b79858f3a02e60bd51d786f55d
sramirezs1598 <sramirezs1598@gmail.com>
2016-07-20 15:43:25 -0400

    Go Green 1.0.4

--- App/Activity1/app/src/main/java/org/mods/goGreen/Climate/ClimateMap.java
+++ App/Activity1/app/src/main/java/org/mods/goGreen/Climate/ClimateMap.java
@@ -5,14 +5,18 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import org.mods.goGreen.Hangman.Hangman;
+import org.mods.goGreen.Quiz.MyQuiz;
 import org.mods.goGreen.R;
 
 /**
  * Created by sergioramirez on 7/18/16.
  */
 public class ClimateMap extends Activity implements View.OnClickListener{
+    public static boolean show=true;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.climate_change_map);
@@ -20,6 +24,8 @@ public class ClimateMap extends Activity implements View.OnClickListener{
         australia.setOnClickListener(this);
         View brazil = findViewById(R.id.brazil_button);
         brazil.setOnClickListener(this);
+        View canada = findViewById(R.id.canada_button);
+        canada.setOnClickListener(this);
         View china = findViewById(R.id.china_button);
         china.setOnClickListener(this);
         View eu = findViewById(R.id.eu_button);
@@ -36,17 +42,21 @@ public class ClimateMap extends Activity implements View.OnClickListener{
         southAfrica.setOnClickListener(this);
         View us = findViewById(R.id.usa_button);
         us.setOnClickListener(this);
-        new AlertDialog.Builder(this)
-                .setItems(R.array.map_explain,
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialoginterface,
-                                                int i) {
-                                startGame(i);
-                            }
-                        })
-                .show();
-        //View un = findViewById(R.id.un);
-        //un.setOnClickListener(this);
+        View un = findViewById(R.id.un_button);
+        un.setOnClickListener(this);
+        if (show) {
+            new AlertDialog.Builder(this)
+                    .setItems(R.array.map_explain,
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialoginterface,
+                                                    int i) {
+                                    startGame(i);
+                                }
+                            })
+                    .show();
+            //View un = findViewById(R.id.un);
+            //un.setOnClickListener(this);
+        }
     }
 
     @Override
@@ -66,6 +76,17 @@ public class ClimateMap extends Activity implements View.OnClickListener{
             case R.id.brazil_button:
                 new AlertDialog.Builder(this)
                         .setItems(R.array.brazil,
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialoginterface,
+                                                        int i) {
+                                        startGame(i);
+                                    }
+                                })
+                        .show();
+                break;
+            case R.id.canada_button:
+                new AlertDialog.Builder(this)
+                        .setItems(R.array.canada,
                                 new DialogInterface.OnClickListener() {
                                     public void onClick(DialogInterface dialoginterface,
                                                         int i) {
@@ -162,6 +183,17 @@ public class ClimateMap extends Activity implements View.OnClickListener{
                                 })
                         .show();
                 break;
+            case R.id.un_button:
+                new AlertDialog.Builder(this)
+                        .setItems(R.array.un,
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialoginterface,
+                                                        int i) {
+                                        startGame(i);
+                                    }
+                                })
+                        .show();
+                break;
             case R.id.back:
                 finish();
                 break;
@@ -169,6 +201,9 @@ public class ClimateMap extends Activity implements View.OnClickListener{
     }
 
     public void startGame(int i) {
+        if (i == 1) {
+           show=false;
+        }
     }
 
 }
